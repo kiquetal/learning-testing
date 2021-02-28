c3283165c191201a24d1f8049675394da605e7e6
Enrique M. Talavera <kiquetal@gmail.com>
2021-02-28 20:06:17 -0300

    Adding request specification

--- readme.md
+++ readme.md
@@ -2,4 +2,7 @@
 
 	Project to be used with Test Automation University courses [Java Path]
 
+#### Tips
+
+	Disable gradle warning, adding the following JVM option: DEFAULT_JVM_OPTS='--illegal-access=deny'
 
--- src/test/java/com/testing/CheckApiTest.java
+++ src/test/java/com/testing/CheckApiTest.java
@@ -1,25 +1,78 @@
 package com.testing;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
+
 public class CheckApiTest {
+
+    private static RequestSpecification requestSpecification;
+
+
+    @BeforeAll
+    public static void setUrl()
+    {
+        requestSpecification= new RequestSpecBuilder()
+                .setBaseUri("http://zippopotam.us")
+                .build();
+    }
+
+    public static Object [] [] zipCodesAndPlaces()
+    {
+        return new Object[][] {
+                { "us","90210","Beverly Hills" },
+                { "us","12345","Schenectady"},
+                {"ca","B2R","Waverley"}
+        };
+
+    }
+
+
+
     @Test
     public void requestUsZipCode90210_logRequestAndLogResponse()
     {
 
-        given().log().all().
-                when().get("http://zippopotam.us/us/90210").
+        given().spec(requestSpecification).log().all().
+                when().get("/us/90210").
                 then().log().body();
      }
 
-     @Test
-    public void requestUsZipCode90210_AssertBody()
+     @ParameterizedTest
+     @MethodSource("zipCodesAndPlaces")
+     public void testFromCollection(String country,String zip, String name)
      {
-         given().
-                 when().get("http://zippopotam.us/us/90210")
+         given().spec(requestSpecification).
+                 pathParam("countryCode",country).
+                 pathParam("zip",zip).
+                 when().get("/{countryCode}/{zip}")
+                 .then().assertThat().body("places[0].'place name'",equalTo(name));
+
+     }
+     @Test
+    public void requestUsZipCode90210_AssertJSONBody()
+     {
+         given().spec(requestSpecification).
+                 when().get("/us/90210")
                  .then().assertThat().body("places[0].'place name'",equalTo("Beverly Hills"));
 
      }
+     @Test
+    public void notIncludeNameInCollections()
+     {
+         given().spec(requestSpecification).
+                 when().get("/us/90210")
+                 .then().assertThat().body("places.'place name'",not(hasItem("Beverly Hills")));
+     }
+
+
 
 
 }
